665cbd9998d437b6486304ab9ed643b1079bbd3f
vince93290 <gameiro93290@gmail.com>
2021-02-28 00:03:07 +0100

    second commit

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: 'arche-de-noe'
+  },
+  plugins: [
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png'
+      }
+    },
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      },
+      __key: 'images'
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/'
+      },
+      __key: 'pages'
+    }
+  ]
+}
