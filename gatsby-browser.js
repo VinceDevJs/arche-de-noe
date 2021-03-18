8d51eb9c41a727acbd6efabb3d3f9c8c9e0f19da
vince93290 <gameiro93290@gmail.com>
2021-03-18 15:15:18 +0100

    test fix slow aos

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-import CMS from 'netlify-cms-app'
-import { fr } from 'netlify-cms-locales'
 import AOS from 'aos'
 import 'aos/dist/aos.css' // You can also use <link> for styles
+import CMS from 'netlify-cms-app'
+import { fr } from 'netlify-cms-locales'
 
 AOS.init()
 
