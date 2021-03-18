421b6ce2a95984416e0a93a8d4d61651658774ba
vince93290 <gameiro93290@gmail.com>
2021-03-18 15:48:06 +0100

    test 4 aos slow

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import waves from '../assets//images/home/waves_2.png'
 import Block1 from '../components/Home/Block1'
 import Block2 from '../components/Home/Block2'
@@ -10,9 +10,13 @@ import Layout from '../Layouts/Layout'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-AOS.init()
-
 const IndexPage = () => {
+  useEffect(() => {
+    if (AOS) {
+      AOS.init()
+    }
+  }, [])
+
   return (
     <Layout>
       <Block1 />
