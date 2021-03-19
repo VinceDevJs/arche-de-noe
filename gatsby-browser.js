8169d0a2311a2301b88ad23c693026ed5861e597
vince93290 <gameiro93290@gmail.com>
2021-03-19 13:58:48 +0100

    test netlify cms language

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,4 @@
-import CMS from 'netlify-cms-app'
-import { fr } from 'netlify-cms-locales'
-
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 AOS.init()
-
-setTimeout(() => CMS.registerLocale('fr', fr), 3000)
