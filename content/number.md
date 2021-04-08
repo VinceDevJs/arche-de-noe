79c5687d04f29246a7879048df2a5fc38af03a00
vince93290 <gameiro93290@gmail.com>
2021-04-08 18:02:37 +0200

    Integration desktop formation (static)

--- content/number.md
+++
@@ -1,7 +0,0 @@
----
-students: 251
-templateKey: homeNumber
-teachers: 15
-teachers2: 22
-curses: 5
----
--- src/utils/functions.js
+++
@@ -1,8 +0,0 @@
-const convertToSlug = function (text) {
-  return text
-    .toLowerCase()
-    .replace(/ /g, '-')
-    .replace(/[^\w-]+/g, '')
-}
-
-module.exports.convertToSlug = convertToSlug
