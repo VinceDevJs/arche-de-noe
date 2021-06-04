0f27c57bbdb9ec1455e0e0d14c1e88878988fa9f
vince93290 <gameiro93290@gmail.com>
2021-06-04 14:10:56 +0200

    test new home block

---
+++ src/components/Home/Block3Bis.js
@@ -0,0 +1,133 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+import BooksImg from './../../assets/images/home/block3-bis/books.png'
+import waves from './../../assets/images/formation/waves_2.png'
+import { breakpoints } from '../../utils/styles.js'
+
+const Block3Bis = () => {
+  return (
+    <Container>
+      <img
+        src={waves}
+        style={{ width: '100%', marginBottom: '2.5em', position: 'absolute', top: '0', left: '0' }}
+        alt=''
+      />
+      <BlockLeft>
+        <Image src={BooksImg} alt='arche de noe' />
+      </BlockLeft>
+
+      <BlockRight>
+        <Title style={{ color: '#00A1C6' }}>Nos Domaines</Title>
+        <Title style={{ color: '#007CAD' }}>d’enseignement</Title>
+        <Separator />
+        <Text>L’Institut l’Arche de Noé a développé au fil des années une réelle expertise dans différents domaines d’enseignement relatif à la langue arabe et à la culture islamique.</Text>
+        <Button to='#matieres'>Les matières enseignées</Button>
+      </BlockRight>
+    </Container>
+  )
+}
+
+export default Block3Bis
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  height: auto;
+  align-items: center;
+  padding: 3em 0;
+
+  @media (max-width: ${breakpoints.s}px) {
+    flex-direction: column;
+  }
+`
+
+export const BlockLeft = styled.div`
+  width: 50%;
+
+  @media (max-width: ${breakpoints.s}px) {
+    width: 100%;
+    margin-bottom: 2em;
+  }
+`
+
+export const Image = styled.img`
+  width: 100%;
+`
+
+export const BlockRight = styled.div`
+  width: 50%;
+  margin-left: 2em;
+
+  @media (max-width: ${breakpoints.s}px) {
+    width: 100%;
+    margin-left: 0;
+    text-align: center;
+  }
+`
+
+export const Title = styled.p`
+  font-family: 'Avenir Next Bold',serif ;
+  margin: -0.3em 0;
+  font-size: 3.5em;
+
+  @media (max-width: ${breakpoints.l}px) {
+    font-size: 2.5em;
+  }
+
+  @media (max-width: ${breakpoints.m}px) {
+    font-size: 2em;
+  }
+
+  @media (max-width: ${breakpoints.m}px) {
+    font-size: 2.5em;
+  }
+  
+`
+
+export const Separator = styled.div`
+  width: 4em;
+  border-top: 3px solid #007CAD;
+  margin: 2.5em 0;
+
+  @media (max-width: ${breakpoints.m}px) {
+    margin: 1.5em 0;
+  }
+
+  @media (max-width: ${breakpoints.s}px) {
+    margin: 2.5em auto;
+  }
+`
+
+export const Text = styled.p`
+  font-family: 'Avenir Next',serif;
+  display: block;
+  max-width: 28em;
+  color: #195780;
+  margin-bottom: 2em;
+
+  @media (max-width: ${breakpoints.l}px) {
+    max-width: 90%;
+  }
+
+  @media (max-width: ${breakpoints.s}px) {
+    max-width: 90%;
+    margin: 0 auto 2em auto;
+  }
+`
+
+export const Button = styled(Link)`
+  border: 1px solid #00A1C6;
+  border-radius: 30px;
+  text-decoration: none;
+  color: #00A1C6;
+  font-size: 1.1em;
+  font-family: 'Avenir Next Bold',serif ;
+  padding: 0.3em 2em;
+  
+  :hover {
+    background-color: #00A1C6;
+    color: white;
+  }
+`
---
+++ src/components/Home/Matieres.js
@@ -0,0 +1,212 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+
+import { breakpoints } from '../../utils/styles.js'
+import arabicIcon from '../../assets/images/home/block3/arabic.png'
+import booksIcon from '../../assets/images/home/block3/books_lamp.png'
+import hadithIcon from '../../assets/images/home/block3/hadith.png'
+import handIcon from '../../assets/images/home/block3/hand.png'
+import quranIcon from '../../assets/images/home/block3/quran.png'
+import salatIcon from '../../assets/images/home/block3/salat.png'
+
+const Matieres = () => {
+  return (
+    <Container id='matieres'>
+      <Title style={{ color: '#00A1C6' }}>Les matières enseignées</Title>
+      <Title style={{ color: '#007CAD' }}>au sein de l’institut</Title>
+
+      <ContentContainer>
+        <ColBox>
+          <ContentBox
+            data-aos='fade-right'
+            data-aos-easing='slide'
+            data-aos-delay='250'
+            data-aos-duration='1200'
+          >
+            <IconBox>
+              <Icon style={{ width: '4em' }} src={quranIcon} />
+            </IconBox>
+            <Text color>Coran</Text>
+          </ContentBox>
+          <Separator />
+          <ContentBox
+            data-aos='fade-right'
+            data-aos-easing='slide'
+            data-aos-delay='250'
+            data-aos-duration='1500'
+          >
+            <IconBox>
+              <Icon style={{ width: '6.5em', marginTop: '1.5em' }} src={salatIcon} />
+            </IconBox>
+            <Text>Sciences Islamiques</Text>
+          </ContentBox>
+        </ColBox>
+
+        <ColBox>
+          <ContentBox
+            data-aos='fade-right'
+            data-aos-easing='slide'
+            data-aos-delay='250'
+            data-aos-duration='1200'
+          >
+            <IconBox>
+              <Icon style={{ width: '5.5em' }} src={arabicIcon} />
+            </IconBox>
+            <Text>Langue Arabe</Text>
+          </ContentBox>
+          <Separator />
+          <ContentBox
+            data-aos='fade-right'
+            data-aos-easing='slide'
+            data-aos-delay='250'
+            data-aos-duration='1500'
+          >
+            <IconBox>
+              <Icon style={{ width: '5em' }} src={handIcon} />
+            </IconBox>
+            <Text color>Grammaire Arabe</Text>
+          </ContentBox>
+        </ColBox>
+
+        <ColBox>
+          <ContentBox
+            data-aos='fade-right'
+            data-aos-easing='slide'
+            data-aos-delay='250'
+            data-aos-duration='1200'
+          >
+            <IconBox>
+              <Icon style={{ width: '5em' }} src={hadithIcon} />
+            </IconBox>
+            <Text color>Hadith</Text>
+          </ContentBox>
+          <Separator />
+          <ContentBox
+            data-aos='fade-right'
+            data-aos-easing='slide'
+            data-aos-delay='250'
+            data-aos-duration='1500'
+          >
+            <IconBox>
+              <Icon style={{ width: '5.5em' }} src={booksIcon} />
+            </IconBox>
+            <Text>Conjugaison Arabe</Text>
+          </ContentBox>
+        </ColBox>
+      </ContentContainer>
+
+      <Button to='/formation'>
+        Accéder à nos formations
+      </Button>
+    </Container>
+  )
+}
+
+export default Matieres
+
+export const Container = styled.div`
+  max-width: 38em;
+  margin: 0 auto 4em auto;
+
+  @media (max-width: ${breakpoints.m}px) {
+    max-width: 35em;
+  }
+
+  @media (max-width: ${breakpoints.s}px) {
+    margin-top: 3em;
+  }
+`
+
+export const Title = styled.p`
+  font-family: 'Avenir Next Bold',serif ;
+  margin: 0;
+  font-size: 2.5em;
+  text-align: center;
+
+  @media (max-width: ${breakpoints.s}px) {
+    font-size: 2em;
+  }
+`
+
+export const ContentContainer = styled.div`
+  margin: 4em 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: ${breakpoints.s}px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+export const ColBox = styled.div`
+  @media (max-width: ${breakpoints.s}px) {
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 4em;
+  }
+`
+
+export const ContentBox = styled.div`
+  width: 10em;
+`
+
+export const IconBox = styled.div`
+  height: 7em;
+  width: 10em;
+  text-align: center;
+
+  @media (max-width: ${breakpoints.s}px) {
+    max-width: 90%;
+    min-height: 10em;
+  }
+`
+
+export const Icon = styled.img`
+  margin: 0 auto;
+
+  @media (max-width: ${breakpoints.s}px) {
+    min-width: 80%;
+  }
+`
+
+export const Text = styled.p`
+  text-align: center;
+  margin: .5em 0;
+  font-family: 'Avenir Next Bold',serif ;
+  color: ${props => props.color ? '#007CAD' : '#00A1C6'}
+`
+
+export const Separator = styled.div`
+  width: 3em;
+  border-top: 3px solid #007CAD;
+  margin: 2.5em auto;
+
+  @media (max-width: ${breakpoints.s}px) {
+    display: none;
+  }
+`
+
+export const Button = styled(Link)`
+  margin: 0 auto;
+  display: block;
+  padding: 0.5em 2em;
+  font-size: 1.1em;
+  border: 1.5px solid #00A1C6;
+  border-radius: 30px;
+  font-family: 'Avenir Next Bold',serif ;
+  color: #00A1C6;
+  width: 13em;
+  text-align: center;
+  text-decoration: none;
+
+  :hover {
+    background-color: #00A1C6;
+    color: white;
+  }
+`
