b447d22191ca29d485132b9cea5c78d2a739a91e
vince93290 <gameiro93290@gmail.com>
2021-06-08 17:17:13 +0200

    test google analytics 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,12 +11,24 @@ module.exports = {
     'gatsby-plugin-sitemap',
     'gatsby-plugin-layout',
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingId: 'G-K7S1720TJ0',
-        // this option places the tracking script into the head of the DOM
-        head: true
-        // other options
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-K7S1720TJ0' // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true
+        }
       }
     },
     {
