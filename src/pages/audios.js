cfdfe8dd02bd83624cea6ad3f63e384150624e8b
vince93290 <gameiro93290@gmail.com>
2021-08-31 17:56:55 +0200

    Audio book Done without style

--- src/pages/audios.js
+++ src/pages/audios.js
@@ -43,19 +43,23 @@ const Audios = () => {
         </BookLinkTitleContainer>
 
         {
-                  allAudioBooks.map(({ node: audioBook }, index) => (
-                    <AudioBookContainer id={audioBook.frontmatter.bookTitle} key={audioBook.frontmatter.bookTitle + index}>
-                      <AudioBookTitle>{audioBook.frontmatter.bookTitle}</AudioBookTitle>
-                      {
-                              audioBook.frontmatter.audio.map((audio, index) => (
-                                <AudioLink key={audio.audioTitle} to={`/audios/${convertToSlug(audioBook.frontmatter.bookTitle)}-${convertToSlug(audio.audioTitle)}`}>
-                                  {index + 1} - {audio.audioTitle}
-                                </AudioLink>
-                              ))
-                          }
-                    </AudioBookContainer>
-                  ))
-              }
+          allAudioBooks.map(({ node: audioBook }, index) => (
+
+            <AudioBookContainer id={audioBook.frontmatter.bookTitle} key={audioBook.frontmatter.bookTitle + index}>
+              <AudioBookTitle>{audioBook.frontmatter.bookTitle}</AudioBookTitle>
+              {
+                audioBook.frontmatter.audio.map((audio, index) => (
+                  <div key={audio.audioTitle} style={{ marginBottom: '0.3em' }}>
+                    <AudioLink to={`/audios/${convertToSlug(audioBook.frontmatter.bookTitle)}-${convertToSlug(audio.audioTitle)}`}>
+                      {index + 1} - {audio.audioTitle}
+                    </AudioLink>
+                    <br />
+                  </div>
+                ))
+                  }
+            </AudioBookContainer>
+          ))
+        }
       </Container>
     </AudioLayout>
   )
