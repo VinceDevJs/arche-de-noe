f627a836adfe5e1c42632b72978b7f802c091515
vince93290 <gameiro93290@gmail.com>
2021-09-19 01:53:16 +0200

    update pop up

--- src/components/GlobalComponents/PopUp.js
+++ src/components/GlobalComponents/PopUp.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
 import closeCross from '../../assets/icons/Close-cross.png'
-import logo from '../../assets/images/inscription/Logo-inscription.svg'
 import { breakpoints } from '../../utils/styles'
-import { Link } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
 
 const PopUp = () => {
   const [showPopUp, setShowPopUp] = useState()
+
+  const image = useStaticQuery(graphql`
+      query {
+          popUp: file(relativePath: { eq: "images/pop-up/pop-up-rentree.png" }) {
+              childImageSharp {
+                  fluid {
+                      ...GatsbyImageSharpFluid
+                  }
+              }
+          }
+      }
+    `)
+
+  console.log(image)
 
   useEffect(() => {
     setShowPopUp(!!((typeof window !== 'undefined' && window.sessionStorage.getItem('popup'))))
@@ -25,30 +39,7 @@ const PopUp = () => {
             <CrossContainer onClick={() => handleClosePopUp()}>
               <img src={closeCross} alt='' />
             </CrossContainer>
-            <Logo src={logo} />
-            <TextContent>
-              L’institut l’Arche de Noé a le plaisir de vous informer que les
-              inscriptions pour l’année 2021 | 2022 se dérouleront les jours suivants
-              :
-            </TextContent>
-            <DateContent>SAMEDI 4 & DIMANCHE 5 SEPTEMBRE 2021
-            </DateContent>
-
-            <ButtonsBox>
-              <Button
-                onClick={() => handleClosePopUp()}
-                to='/inscription#main_inscription'
-              >
-                Connaître les modalités
-              </Button>
-              <Button
-                to='/formation'
-                color='true'
-                onClick={() => handleClosePopUp()}
-              >
-                Nos formations
-              </Button>
-            </ButtonsBox>
+            <ImagePopUp fluid={image.popUp.childImageSharp.fluid} />
           </BoxContainer>
         </Container>}
     </>
@@ -90,11 +81,11 @@ export const BoxContainer = styled.div`
   z-index: 1000;
   background-color: #297daf;
   width: 70%;
-  padding: 2em 0;
+  padding: 0;
   max-width: 40em;
-  height: 21em;
+  height: 23em;
   border: 6px solid white;
-  border-radius: 15px;
+  border-radius: 21px;
 
   @media (max-width: ${breakpoints.m}px) {
     width: 85%;
@@ -104,7 +95,7 @@ export const BoxContainer = styled.div`
   }
 
   @media (max-width: ${breakpoints.s}px) {
-    max-height: 23em;
+    max-height: 12em;
     margin-top: 4em;
   }
 `
@@ -123,97 +114,18 @@ export const CrossContainer = styled.div`
   height: 2.3em;
   border: 4px solid white;
   color: white;
+  z-index: 100;
   
   img {
     width: 15px;
   }
 `
 
-export const Logo = styled.img`
+export const ImagePopUp = styled(Image)`
   position: absolute;
-  width: 17em;
-  z-index: 10;
-  opacity: 0.4;
-`
-
-export const TextContent = styled.p`
-  display: inline-block;
-  color: white;
-  font-size: 1.3em;
-  text-align: center;
-  margin: 0 auto 0.4em;
-  max-width: 80%;
-  z-index: 11;
-
-  @media (max-width: ${breakpoints.l}px) {
-    
-  }
-
-  @media (max-width: ${breakpoints.s}px) {
-    width: 90%;
-    font-size: 1.1em;
-  }
-`
-
-export const DateContent = styled.p`
-  display: inline-block;
-  font-size: 1.5em;
-  text-align: center;
-  color: #6cdcef;
-  font-family: 'Avenir Next Bold', monospace;
-  max-width: 85%;
-  z-index: 11;
-
-  @media (max-width: ${breakpoints.l}px) {
-   
-  }
-
-  @media (max-width: ${breakpoints.m}px) {
-    width: 60%;
-  }
-
-  @media (max-width: ${breakpoints.s}px) {
-    width: 90%;
-  }
-`
-
-export const ButtonsBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  align-content: space-between;
-  width: 35em;
-  margin: 1em auto 0;
-  z-index: 11;
-
-  @media (max-width: ${breakpoints.m}px) {
-    flex-direction: column;
-  }
-
-  @media (max-width: ${breakpoints.s}px) {
-    width: 90%;
-  }
-`
-
-export const Button = styled(Link)`
-  border-radius: 40px;
-  font-family: 'Avenir Next Bold', monospace;
-  border: ${props => (props.color ? '2px solid #6CDCEF' : '2px solid white')};
-  background-color: ${props => (props.color ? '#6CDCEF' : 'transparent')};
-  width: 14em;
-  padding: 0.4em 0;
-  text-align: center;
-  font-size: 1.2em;
-  color: ${props => (props.color ? '#007CAD' : 'white')};
-  text-decoration: none;
-
-  @media (max-width: ${breakpoints.m}px) {
-    margin-bottom: ${props => (props.color ? '0' : '1em')};
-  }
-
-  :hover {
-    border: ${props =>
-  !props.color ? '2px solid #6CDCEF' : '2px solid white'};
-    cursor: pointer;
-  }
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
 `
